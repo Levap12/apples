3d3ccd76d8cc4a4d6e3d85e0a0bba83a1b085d6d
Levap12 <123398140+Levap12@users.noreply.github.com>
2026-08-10 14:37:18 +0500

    Add PWA manifest, service worker and icons

---
+++ sw.js
@@ -0,0 +1,15 @@
+const CACHE = 'cerdic-pro-v1';
+const ASSETS = ['./', './index.html', './app.webmanifest', './icon-192.png', './icon-512.png'];
+
+self.addEventListener('install', event => {
+  event.waitUntil(caches.open(CACHE).then(cache => cache.addAll(ASSETS)));
+});
+
+self.addEventListener('activate', event => {
+  event.waitUntil(self.clients.claim());
+});
+
+self.addEventListener('fetch', event => {
+  if (event.request.method !== 'GET') return;
+  event.respondWith(caches.match(event.request).then(hit => hit || fetch(event.request)));
+});
