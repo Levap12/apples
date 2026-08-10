e25dc289f2734d7e318d15eb65f35e4c8ebd86c2
Levap12 <123398140+Levap12@users.noreply.github.com>
2026-08-10 14:39:55 +0500

    Refresh CEDRIC app cache

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'cerdic-pro-v1';
+const CACHE = 'cedric-pro-v2';
 const ASSETS = ['./', './index.html', './app.webmanifest', './icon-192.png', './icon-512.png'];
 
 self.addEventListener('install', event => {
