d114cf16c4d7618ee147cf6096338e80d1bca2f1
Levap12 <123398140+Levap12@users.noreply.github.com>
2026-08-12 15:40:38 +0500

    Add hacker design and batch reveal mode

--- sw.js
+++ sw.js
@@ -1,15 +1,5 @@
-const CACHE = 'cedric-pro-v2';
-const ASSETS = ['./', './index.html', './app.webmanifest', './icon-192.png', './icon-512.png'];
-
-self.addEventListener('install', event => {
-  event.waitUntil(caches.open(CACHE).then(cache => cache.addAll(ASSETS)));
-});
-
-self.addEventListener('activate', event => {
-  event.waitUntil(self.clients.claim());
-});
-
-self.addEventListener('fetch', event => {
-  if (event.request.method !== 'GET') return;
-  event.respondWith(caches.match(event.request).then(hit => hit || fetch(event.request)));
-});
+const CACHE='cedric-matrix-v1';
+const ASSETS=['./','./index.html','./app.webmanifest','./icon-192.png','./icon-512.png'];
+self.addEventListener('install',event=>event.waitUntil(caches.open(CACHE).then(cache=>cache.addAll(ASSETS))));
+self.addEventListener('activate',event=>event.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(key=>key!==CACHE).map(key=>caches.delete(key)))).then(()=>self.clients.claim())));
+self.addEventListener('fetch',event=>{if(event.request.method==='GET')event.respondWith(caches.match(event.request).then(hit=>hit||fetch(event.request)))});
