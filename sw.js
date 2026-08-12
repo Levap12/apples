42f22d5b39dc45e3b2acab4f43f63d6b8ce5e159
Levap12 <123398140+Levap12@users.noreply.github.com>
2026-08-12 15:58:21 +0500

    Refresh matrix animation cache

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE='cedric-matrix-v2';
+const CACHE='cedric-matrix-v3';
 const ASSETS=['./','./index.html','./app.webmanifest','./icon-192.png','./icon-512.png'];
 self.addEventListener('install',event=>event.waitUntil(caches.open(CACHE).then(cache=>cache.addAll(ASSETS)).then(()=>self.skipWaiting())));
 self.addEventListener('activate',event=>event.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(key=>key!==CACHE).map(key=>caches.delete(key)))).then(()=>self.clients.claim())));
